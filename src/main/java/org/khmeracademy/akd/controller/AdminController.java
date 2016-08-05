c66f5cce1b1322bebdab2261ffe8f62a809403eb
Chivorns <chivorn@live.com>
2016-08-05 09:07:11 +0700

    Update Admin

---
+++ src/main/java/org/khmeracademy/akd/controller/AdminController.java
@@ -0,0 +1,20 @@
+package org.khmeracademy.akd.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class AdminController {
+	
+	
+	
+	@RequestMapping(value="/admin",method=RequestMethod.GET)
+	public String findAll(){
+		return "admin/dashboard";
+	}
+	
+	
+}
