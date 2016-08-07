f58efbf7ca8f7bea7bc652cc9e8ada52c26bf5e7
KimBunhong <bunhongkim93@gmail.com>
2016-08-07 16:37:34 +0700

    from doctor kim bunhong

--- src/main/java/org/khmeracademy/akd/controller/AdminController.java
+++ src/main/java/org/khmeracademy/akd/controller/AdminController.java
@@ -15,6 +15,35 @@ public class AdminController {
 	public String findAll(){
 		return "admin/dashboard";
 	}
+	@RequestMapping(value="/admin/slide",method=RequestMethod.GET)
+	public String slide(){
+		return "admin/slide";
+	}
 	
+	@RequestMapping(value="/admin/user",method=RequestMethod.GET)
+	public String user(){
+		return "admin/user";
+	}
+	
+	@RequestMapping(value="/admin/comment",method=RequestMethod.GET)
+	public String comment(){
+		return "admin/comment";
+	}
+	
+	@RequestMapping(value="/admin/feedback",method=RequestMethod.GET)
+	public String feedback(){
+		return "admin/feedback";
+	}
+	
+	@RequestMapping(value="/admin/main-category",method=RequestMethod.GET)
+	public String mainCategory(){
+		return "admin/main-category";
+	}
+	
+	@RequestMapping(value="/admin/sub-category",method=RequestMethod.GET)
+	public String subCategory(){
+		return "admin/sub-category";
+	}
+
 	
 }
