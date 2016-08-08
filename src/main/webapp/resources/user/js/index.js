7ebcb033057a7d268b6b24e9987df1f24aade0e9
Chivorns <chivorn@live.com>
2016-08-08 13:36:03 +0700

    Update Dynamic Link on slide.

--- src/main/webapp/resources/user/js/index.js
+++ src/main/webapp/resources/user/js/index.js
@@ -5,12 +5,9 @@ app.controller('indexCtrl', function($scope,$sce) {
     
     $scope.slides = [
                      {"id":1, "url":'https://onedrive.live.com/embed?cid=41468BA873DB6994&resid=41468BA873DB6994%21434&authkey=AABcOreYLL7cv-I&em=2&wdAr=1.7777777777777777'},
-                     {"id":2, "url":'https://onedrive.live.com/view.aspx?cid=41468ba873db6994&page=view&resid=41468BA873DB6994!442&parId=41468BA873DB6994!103&app=PowerPoint'}
+                     {"id":2, "url":'https://onedrive.live.com/embed?cid=41468BA873DB6994&resid=41468BA873DB6994%21442&authkey=ALAtWEYG0XCIT68&em=2'}
                     ];
-    
-    
-
-    
+       
 
     $scope.getSlideURL = function(slide){
 		// $http({
