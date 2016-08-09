122fb33db97726a17b7fd4613dd294bc2a8c25f2
Chivorns <chivorn@live.com>
2016-08-09 23:40:46 +0700

    Updating Page Detail but not ready. Just push for Chem Minea.

--- src/main/java/org/khmeracademy/akd/controller/UserController.java
+++ src/main/java/org/khmeracademy/akd/controller/UserController.java
@@ -2,7 +2,6 @@ package org.khmeracademy.akd.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
