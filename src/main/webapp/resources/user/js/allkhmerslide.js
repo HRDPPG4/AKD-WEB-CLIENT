b885a08dc711c4a90b8d24182152776445ebaf20
mineachem <minea.chem@gmail.com>
2016-08-19 14:26:54 +0700

    add history

--- src/main/webapp/resources/user/js/allkhmerslide.js
+++ src/main/webapp/resources/user/js/allkhmerslide.js
@@ -105,9 +105,4 @@ jQuery(document).ready(function($) {
 });
 
 
-//profile
-//auto expand textarea
-function adjust_textarea(h) {
-    h.style.height = "20px";
-    h.style.height = (h.scrollHeight)+"px";
-}
+
